218ceff1a10516d7623a00f47e8161eb94897ae7
Idt Danny <idtdanny@gmail.com>
2023-11-19 21:20:01 +0000

    AirBnB_clone_v4 - Copied everything from AirBnB_clone_v3

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python3
+"""
+Contains the TestDBStorageDocs and TestDBStorage classes
+"""
+
+from datetime import datetime
+import inspect
+import models
+from models.engine import db_storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import json
+import os
+import pep8
+import unittest
+DBStorage = db_storage.DBStorage
+classes = {"Amenity": Amenity, "City": City, "Place": Place,
+           "Review": Review, "State": State, "User": User}
+
+
+class TestDBStorageDocs(unittest.TestCase):
+    """Tests to check the documentation and style of DBStorage class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
+    
+    def setUp(self):
+        """Set up for the tests"""
+        self.storage = DBStorage()
+        self.session = self.storage._DBStorage__session
+
+    def tearDown(self):
+        """Clean up resources after tests"""
+        self.session.rollback()
+        self.session.close()
+
+    def test_get(self):
+        """Test the get method"""
+        # Add a test object to the database
+        test_obj = BaseModel()
+        self.session.add(test_obj)
+        self.session.commit()
+
+        # Test get with valid class and ID
+        obj = self.storage.get(BaseModel, test_obj.id)
+        self.assertEqual(obj.id, test_obj.id)
+
+        # Test get with invalid ID
+        obj = self.storage.get(BaseModel, "12345")
+        self.assertIsNone(obj)
+
+        # Test get with None as class
+        obj = self.storage.get(None, test_obj.id)
+        self.assertIsNone(obj)
+
+        # Test get with None as ID
+        obj = self.storage.get(BaseModel, None)
+        self.assertIsNone(obj)
+
+    def test_count(self):
+        """Test the count method"""
+        # Add test objects to the database
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        self.session.add_all([obj1, obj2])
+        self.session.commit()
+
+        # Test count with specific class
+        count = self.storage.count(BaseModel)
+        self.assertEqual(count, 2)
+
+        # Test count with no class (should count all objects)
+        count = self.storage.count()
+        self.assertEqual(count, 2)
+
+    def test_pep8_conformance_db_storage(self):
+        """Test that models/engine/db_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_db_storage(self):
+        """Test tests/test_models/test_db_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_engine/\
+test_db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_db_storage_module_docstring(self):
+        """Test for the db_storage.py module docstring"""
+        self.assertIsNot(db_storage.__doc__, None,
+                         "db_storage.py needs a docstring")
+        self.assertTrue(len(db_storage.__doc__) >= 1,
+                        "db_storage.py needs a docstring")
+
+    def test_db_storage_class_docstring(self):
+        """Test for the DBStorage class docstring"""
+        self.assertIsNot(DBStorage.__doc__, None,
+                         "DBStorage class needs a docstring")
+        self.assertTrue(len(DBStorage.__doc__) >= 1,
+                        "DBStorage class needs a docstring")
+
+    def test_dbs_func_docstrings(self):
+        """Test for the presence of docstrings in DBStorage methods"""
+        for func in self.dbs_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test the FileStorage class"""
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_all_returns_dict(self):
+        """Test that all returns a dictionaty"""
+        self.assertIs(type(models.storage.all()), dict)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_all_no_class(self):
+        """Test that all returns all rows when no class is passed"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_new(self):
+        """test that new adds an object to the database"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
---
+++ web_flask/templates/README.md
@@ -0,0 +1,1 @@
+Template directory for web_flask
